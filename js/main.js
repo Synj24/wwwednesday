1237369cc23896e6b1260aeb8b320282381c6f27
M.D.Gregory <matthew.gregory@hotmail.co.uk>
2017-12-20 15:46:22 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -60,39 +60,17 @@
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 6);
+/******/ 	return __webpack_require__(__webpack_require__.s = 14);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 6:
+/***/ 14:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(7);
+(function webpackMissingModule() { throw new Error("Cannot find module \"./assets/js/index.js\""); }());
 
-
-/***/ }),
-
-/***/ 7:
-/***/ (function(module, exports) {
-
-var text = document.getElementById("not-wednesday");
-var video = document.getElementById("jimmy");
-var frog = document.getElementById("frog");
-
-var date = new Date();
-var day = date.getDay();
-
-if (day == 3) {
-  text.remove();
-  frog.remove();
-  document.body.style.backgroundColor = "#000";
-  console.log("It is wednesday my dudes :)");
-} else {
-  video.remove();
-}
 
 /***/ })
 
 /******/ });
-//# sourceMappingURL=main.js.map
